45063510e3ea1e9d418514d75177aa0171750eca
harshithareddy603 <harshitareddy919@gmail.com>
2026-08-29 22:24:45 +0530

    drtyujhgvftyu

--- plugins/withAndroidPip.js
+++ plugins/withAndroidPip.js
@@ -1,6 +1,19 @@
-const { withMainActivity } = require('@expo/config-plugins');
+const { withMainActivity, withAndroidManifest } = require('@expo/config-plugins');
+
+function withAndroidPipManifest(config) {
+  return withAndroidManifest(config, (config) => {
+    const mainActivity = config.modResults.manifest.application[0].activity.find(
+      (activity) => activity['$']['android:name'] === '.MainActivity'
+    );
+    if (mainActivity) {
+      mainActivity['$']['android:supportsPictureInPicture'] = 'true';
+    }
+    return config;
+  });
+}
 
 module.exports = function withAndroidPip(config) {
+  config = withAndroidPipManifest(config);
   return withMainActivity(config, (config) => {
     let mainActivity = config.modResults.contents;
 
